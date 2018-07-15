ba1ec74dd2edcb00784ebc00ee736437ed9a677c
josh-jhs8 <josh.jhs8@gmail.com>
2018-07-15 12:51:05 +0100

    Set up connection protocols

--- Challenger/Program.cs
+++ Challenger/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Challenger.Logic;
+using Challenger.Model;
+using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Challenger
@@ -10,6 +11,50 @@ namespace Challenger
     {
         static void Main(string[] args)
         {
+            var listener = ListenForConnections();
+            Console.WriteLine("Press Enter to kill server");
+            Console.ReadLine();
+        }
+
+        private static Task ListenForConnections()
+        {
+            return Task.Run(() =>
+            {
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+                var ipAddress = host.AddressList[host.AddressList.Length - 1];
+                var localEndpoint = new IPEndPoint(ipAddress, 2092);
+
+                using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    socket.Bind(localEndpoint);
+
+                    while (true)
+                    {
+                        socket.Listen(100);
+                        var connection = socket.Accept();
+                        var task = ManageConnection(connection);
+                    }
+                }
+            });
+        }
+
+        private static Task ManageConnection(Socket connection)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("New Connection means new Challenge!");
+                var config = ChallengeConfiguration.GetChallengeConfiguration(@"C:\Users\joshj\Source\Challenger\Challenger\ChallengeConfiguration.json");
+                var manager = new ChallengeManager(config);
+                while (true)
+                {
+                    byte[] data = new byte[1000000];
+                    var dataSize = connection.Receive(data);
+                    byte[] command = new byte[dataSize];
+                    Array.Copy(data, command, dataSize);
+                    var response = manager.ProcessCommand(command);
+                    connection.Send(response);
+                }
+            });
         }
     }
 }
