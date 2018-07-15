8102f79404139afc554599df73b501c04975555f
josh-jhs8 <josh.jhs8@gmail.com>
2018-07-15 10:42:08 +0100

    Set up model based on json file

---
+++ Challenger/Model/ChallengeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Challenger.Model
+{
+    using Map;
+    using Newtonsoft.Json;
+    using Player;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class ChallengeConfiguration
+    {
+        public List<System> Systems { get; set; }
+
+        public List<Ship> Ships { get; set; }
+
+        public static ChallengeConfiguration GetChallengeConfiguration(string path)
+        {
+            var json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<ChallengeConfiguration>(json);
+        }
+    }
+}
